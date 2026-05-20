0eb4666a9eabf4419219c832a7fe5fb96a1bd324
payalsr <rajoriapayal13@gmil.com>
2026-05-20 17:03:09 +0530

    mcp server and client using stdio

---
+++ 01_MCP_Introduction/client.py
@@ -0,0 +1,27 @@
+import asyncio
+from fastmcp import Client
+from fastmcp.client.transports.stdio import StdioTransport   # Change in the import path for StdioTransport
+
+async def main():
+    transport = StdioTransport(
+        #"python"
+        "D:\\L3April2026\\.venv\\Scripts\\python.exe",           # ✅ command
+        ["server.py"]       # ✅ args (separate list)
+    )
+
+    async with Client(transport) as client:
+        tools = await client.list_tools()
+        print("Available tools:", tools)
+        
+        # Call the "greet" tool with the name "Alice"
+        result = await client.call_tool(
+            "greet",
+            {"name": "Sharad"}
+        )
+    
+        print("Result:", result)
+
+if __name__ == "__main__":
+    asyncio.run(main())
+    
+    
